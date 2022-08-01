d7d3ab6f51058d8c4c4a7069b310a260dcd792d3
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2022-08-01 15:22:08 +0100

    initial setup of content pages
    set up the pages ready to add more content at a later date. needed to get the url paths to work before i got started on the pages content

--- settings/urls.py
+++ settings/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from index import views
 
 urlpatterns = [
     path('', views.Index, name='index'),
+    path('Models/', include('BlenderModeling.urls')),
+    path('Electronics/', include('Electronics.urls')),
+    path('Twitch/', include('twitch.urls')),
     path('admin/', admin.site.urls),
 ]
